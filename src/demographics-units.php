d78a8fc8d3dceafda06183c7bdeef3722d8ce6a3
njoguamos <njoguamos@icloud.com>
2019-02-17 08:48:51 +0300

    Added demographics (PHP)

---
+++ src/demographics-units.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+  [
+      "name"         => "Mombasa",
+      "capital"      => "Mombasa (City)",
+      "code"         => 1,
+      "sub_counties" => [
+          "Changamwe","Jomvu","Kisauni","Likoni","Mvita","Nyali",
+      ],
+      "constituencies" => [
+          "Changamwe","Jomvu","Kisauni","Likoni","Mvita","Nyali"
+      ]
+  ]
+];
